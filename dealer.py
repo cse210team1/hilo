eff46051d83bc095dff20bfac62e6445f08d0f8f
Spencer Wadsworth <sjw1999@byui.edu>
2021-03-04 18:24:15 -0500

    added Dealer class and __main__ function

--- dealer.py
+++ dealer.py
@@ -1,1 +1,19 @@
+import random
 
+class Dealer():
+
+    def __init__(self):
+        self.current_card = random.randint(1, 13)
+        self.last_card = random.randint(1, 13)
+
+    def deal_card(self):
+        self.last_card = current_card
+        self.current_card = random.randint(1, 13)
+
+    def compare_to_last_card(self):
+        if self.current_card > self.last_card:
+            return "h"
+        elif self.current_card < self.last_card:
+            return "l"
+        else:
+            return "e"
