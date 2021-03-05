230ff193e841036138389cdc649373438725f922
Spencer Wadsworth <sjw1999@byui.edu>
2021-03-05 15:44:24 -0500

    added display function

--- director.py
+++ director.py
@@ -19,7 +19,7 @@ class Director:
             self (Director): an instance of Director.
         """
         print(f"Your score is: {self.score} ")
-        print(f"The card is : {self.dealer.current_card}" )
+        print(f"The card is: {Director.display(self.dealer.current_card)}" )
         while self.keep_playing:
             self.get_inputs()
             self.do_updates()
@@ -38,7 +38,7 @@ class Director:
             self.score += -75
 
     def do_outputs(self):
-        print(f"The card is : {self.dealer.current_card}" )
+        print(f"The card is: {Director.display(self.dealer.current_card)}" )
         print(f"Your score is: {self.score} ")
 
         if self.score > 0:
@@ -47,3 +47,18 @@ class Director:
         else: 
             print("You lose")
             self.keep_playing = False
+
+    @staticmethod
+    def display(card):
+        if card == 1:
+            display = "A"
+        elif card == 11:
+            display = "J"
+        elif card == 12:
+            display = "Q"
+        elif card == 13:
+            display = "K"
+        else:
+            display = card
+
+        return display
